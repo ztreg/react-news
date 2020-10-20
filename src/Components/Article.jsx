223c3dba26dac7ef7406d630a532d6ce49374ca7
Jonas Gertz <Geertzo@live.se>
2020-10-20 13:35:08 +0200

    updates

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Article(props) {
+
+  function handleOnClick () {
+    props.setExpandedItem(props.index)
+  }
 
   return (
   <div className="col-md-4"> 
     <div className="card" style={{maxWidth: "50rem"}}>
-      <img src={props.articleitem.urlToImage} alt="" className="img-fluid"/>
+      {props.expandedItem === props.index && (
+        <>
+          <img src={props.articleitem.urlToImage} alt="" className="img-fluid maximg"/>
+        </>
+      )}
       <div className="card-body">
-      <h5 className="card-title">{props.articleitem.title}</h5>
-  
+      <h5 className="card-title" onClick={handleOnClick}>{props.articleitem.title}</h5>
+        <p className="card-text">{props.articleitem.description}</p>
         <p className="card-text">{props.articleitem.content}</p>
-       
         <a className="btn btn-primary" target="_blank" rel="noopener noreferrer" href={props.articleitem.url} alt="to site">View Article</a>
         <p className="card-title">Written by: {props.articleitem.author}</p>
         <p className="card-text">PublishedAt {props.articleitem.publishedAt}</p>
--- src/Components/Newsarticles.jsx
+++ src/Components/Newsarticles.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Article from './Article'
 
 export default function Newsarticles(props) {
+
+  const [expandedItem, setExpandedItem] = useState(null)
+
   return (
-    <div className="row">
+    <div className="row bg-white">
+      EXPANDED ITEM: {expandedItem}
       {props.articles.map((item, index) => {
-      return <Article
-        key={index} 
-        articleitem={item}
-      />
+      return <Article 
+        key={index}
+        index={index} 
+        articleitem={item} 
+        expandedItem={expandedItem} 
+        setExpandedItem={setExpandedItem}/>
     })}
     </div>
   )
